865f9e05477c2088fe6a1446999a54f4b360ec76
gauravjaisinghani <gauravjaisinghani927@gmail.com>
2024-09-01 04:17:20 +0530

    logo image out of public

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import logo from "./something.png"
 
 export default function Home() {
   return (
@@ -6,7 +7,7 @@ export default function Home() {
       <div className="relative mb-4">
         <Image
           className="relative"
-          src="/something.png"
+          src={logo}
           alt="Next.js Logo"
           width={180}
           height={37}
