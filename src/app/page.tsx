8edfa3199748026f231d49843ab74bb2f6518688
gauravjaisinghani <gauravjaisinghani927@gmail.com>
2024-09-01 15:47:06 +0530

    content section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import logo from "./something.png"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-4 bg-[#6e61e9]">
+    <main className="flex min-h-screen flex-col text-white items-center p-4 bg-[#6e61e9]">
       <div className="relative mb-4">
         <Image
           className="relative"
@@ -14,7 +14,15 @@ export default function Home() {
           priority
         />
       </div>
-      <hr className="w-full text-white"/>
+      <hr className="w-full"/>
+      <div className="w-full items-center flex flex-col">
+        <p className="m-8">Content Section Goes here</p>
+        <div className="flex w-full justify-evenly flex-wrap">
+          <div className="mb-8 rounded-md min-w-80 h-80 border border-solid border-white bg-white shadow-md"></div>
+          <div className="mb-8 rounded-md min-w-80 h-80 border border-solid border-white bg-white shadow-md"></div>
+          <div className="mb-8 rounded-md min-w-80 h-80 border border-solid border-white bg-white shadow-md"></div>
+        </div>
+      </div>
     </main>
   );
 }
